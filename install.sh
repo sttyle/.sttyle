7a4e9505d9ded4acdcfdea4c2a91fa203478b0a5
Alex <arpchesebro@gmail.com>
2014-12-05 14:35:53 -0800

    updating things

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 # Seting screenrc and tmux.conf
 if [ ! -f ~/.screenrc ]; then
-    ln -s ~/.sttyle/screenrc ~/.screenrc
+    ln -s ~/.sttyle/dotfiles/screenrc ~/.screenrc
 fi
 if [ ! -f ~/.tmux.conf ]; then
-    ln -s ~/.sttyle/tmux.conf ~/.tmux.conf
+    ln -s ~/.sttyle/dotfiles/tmux.conf ~/.tmux.conf
 fi
 
 # Setup vim
@@ -13,26 +13,24 @@ if [ ! -d ~/.vim ]; then
     ln -s ~/.sttyle/vim ~/.vim
 fi
 if [ ! -f ~/.vimrc ]; then
-    ln -s ~/.sttyle/vimrc ~/.vimrc
+    ln -s ~/.sttyle/dotfiles/vimrc ~/.vimrc
 fi
 if [ ! -f ~/.vrapperrc ]; then
-    ln -s ~/.sttyle/vrapperrc ~/.vrapperrc
+    ln -s ~/.sttyle/dotfiles/vrapperrc ~/.vrapperrc
 fi
 
 # Link gitconfig
 if [ ! -f ~/.gitconfig ]; then
-    ln -s ~/.sttyle/git/gitconfig ~/.gitconfig
+    ln -s ~/.sttyle/dotfiles/gitconfig ~/.gitconfig
 fi
 
 if [ -f ~/.bashrc ]; then
     grep -q sttylerc ~/.bashrc && echo "bashrc already installed" || echo 'source ~/.sttyle/sttylerc' >> ~/.bashrc
 else
-    touch ~/.bashrc
     echo 'source ~/.sttyle/sttylerc' >> ~/.bashrc
 fi
 if [ -f ~/.zshrc ]; then
     grep -q sttylerc ~/.zshrc && echo "zshrc already installed" || echo 'source ~/.sttyle/sttylerc' >> ~/.zshrc
 else
-    touch ~/.zshrc
     echo 'source ~/.sttyle/sttylerc' >> ~/.zshrc
 fi
